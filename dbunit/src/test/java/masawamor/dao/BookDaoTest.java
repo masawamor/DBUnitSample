3727c1292c72346fa3b8369c85784ec6dec09eeb
Masawa Morishita <project.masawa@gmail.com>
2022-10-21 09:55:48 +0900

    Resolve getting db connection

--- dbunit/src/test/java/masawamor/dao/BookDaoTest.java
+++ dbunit/src/test/java/masawamor/dao/BookDaoTest.java
@@ -2,18 +2,19 @@ package masawamor.dao;
 
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.xml.XmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
-import org.junit.Test;
+import org.h2.tools.RunScript;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import masawamor.entity.Book;
 
@@ -23,14 +24,14 @@ public class BookDaoTest {
 	static IDatabaseConnection connection;
 
 	private static final String JDBC_DRIVER = "org.h2.Driver";
-	private static final String URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+	private static final String URL = "jdbc:h2:mem:test_db;DB_CLOSE_DELAY=-1";
 	private static final String USER = "sa";
 	private static final String PASSWORD = "";
 	
-//	@BeforeClass
-//    public static void createSchema() throws Exception {
-//        RunScript.execute(JDBC_URL, USER, PASSWORD, "data/schema.sql", null, false);
-//    }
+	@BeforeClass
+    public static void createSchema() throws Exception {
+        RunScript.execute(URL, USER, PASSWORD, "resources/schema.sql", null, false);
+    }
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -38,10 +39,10 @@ public class BookDaoTest {
 		databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
 		connection = databaseTester.getConnection();
 		
-		PreparedStatement pstmt = connection.getConnection().prepareStatement(
-				"CREATE TABLE books (id int, title varchar, authorid int);"
-				);
-		pstmt.execute();
+//		PreparedStatement pstmt = connection.getConnection().prepareStatement(
+//				"CREATE TABLE books (id int, title varchar, authorid int);"
+//				);
+//		pstmt.execute();
 	}
 
 	@AfterAll
@@ -53,7 +54,7 @@ public class BookDaoTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		XmlDataSet dataSet = readXmlDataSet("resources/BookDaoTset/actual.xml");
+		XmlDataSet dataSet = readXmlDataSet("resources/BookDao/actual.xml");
 		//FlatXmlDataSet dataSet = new FlatXmlDataSet(new FileInputStream("resources/BookDaoTset/actual.xml"));
 		databaseTester.setDataSet(dataSet);
 		databaseTester.onSetup();
@@ -65,9 +66,10 @@ public class BookDaoTest {
 
 	@Test
 	public void test() throws Exception {
+		System.out.println("@1");
 		Connection conn = connection.getConnection();
 		Book book = new BookDao(conn).selectOne(1);
-		System.out.println("@");
+		System.out.println("@2");
 	}
 	
 	private XmlDataSet readXmlDataSet(String path) throws Exception {
